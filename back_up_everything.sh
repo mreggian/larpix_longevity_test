81baa71bd19bf5327ddb02cd7b2e4fd8b40309ef
Marina Reggiani-Guzzo <mreggian@g.syr.edu>
2026-04-21 10:40:18 -0400

    simplifying script
    Merging backup commands for vdda, vddd, idda, iddd, mean_pedestal and packets, to include all tiles per command

--- back_up_everything.sh
+++ back_up_everything.sh
@@ -18,13 +18,13 @@
 # Option 2: Last 2 minutes, time_interval="start:-2m"
 # Option 3: Last 3 days, time_interval="start:-3d"
 # Option 4: Between timestamp1 and timestamp2, time_interval="start: YYYY-MM-DDHH:MM:SSZ, stop: YYY-MM-DDHH:MM:SSZ"
-time_interval="start:-60m"
+time_interval="start: 2025-12-15T14:00:00Z, stop: -0m" # everything from the beginning
 
 # This tag allows you to add extra information on the file name, eg: myfile_60m.csv | You can leave it empty if you want
-file_name_tag="60m"
+file_name_tag="2026-04-21"
 
 # Please make sure the directory path is correct, and DO NOT include the last / in the path
-dir_path="/home/syr-neutrino/Desktop/InfluxDB_Backups" 
+dir_path="/home/syr-neutrino/Desktop/InfluxDB_Backups/csv_files" 
 
 # Choose which variables you want to create a backup for. By default, back up all variables!
 # 0 = labjack | room temperature
@@ -38,7 +38,13 @@ dir_path="/home/syr-neutrino/Desktop/InfluxDB_Backups"
 # 8 = ctc | voltage
 # 9 = ctc | pressure
 # 10 = ctc | heater
-chosen_variables=(1 2 3 4 5 6 7 8 9 10)
+# 11 = pacman_boards | vdda all tiles
+# 12 = pacman_boards | vddd all tiles
+# 13 = pacman_boards | idda all tiles
+# 14 = pacman_boards | iddd all tiles
+# 15 = pacman_boards | mean_pedestal all tiles
+# 16 = pacman_boards | packets all tiles
+chosen_variables=(1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16)
 
 # Include here the ROOT token of the InfluxDB
 export influx_token_marina=""
@@ -61,6 +67,12 @@ variables=(
     "r._measurement==\"ctc\" and r._field==\"voltage\""
     "r._measurement==\"ctc\" and r._field==\"pressure\""
     "r._measurement==\"ctc\" and r._field==\"heater\""
+    "r._measurement==\"pacman_boards\" and r._field==\"vdda\""
+    "r._measurement==\"pacman_boards\" and r._field==\"vddd\""
+    "r._measurement==\"pacman_boards\" and r._field==\"idda\""
+    "r._measurement==\"pacman_boards\" and r._field==\"iddd\""
+    "r._measurement==\"pacman_boards\" and r._field==\"mean_pedestal\""
+    "r._measurement==\"pacman_boards\" and r._field==\"packets\""
 )
 columns=(
     "\"_time\",\"_value\",\"_field\",\"_measurement\",\"id\""
@@ -74,6 +86,12 @@ columns=(
     "\"_time\",\"_value\",\"_field\",\"_measurement\""
     "\"_time\",\"_value\",\"_field\",\"_measurement\""
     "\"_time\",\"_value\",\"_field\",\"_measurement\""
+    "\"_time\",\"_value\",\"_field\",\"_measurement\",\"tile\""
+    "\"_time\",\"_value\",\"_field\",\"_measurement\",\"tile\""
+    "\"_time\",\"_value\",\"_field\",\"_measurement\",\"tile\""
+    "\"_time\",\"_value\",\"_field\",\"_measurement\",\"tile\""
+    "\"_time\",\"_value\",\"_field\",\"_measurement\",\"tile\",\"channel_id\""
+    "\"_time\",\"_value\",\"_field\",\"_measurement\",\"tile\",\"channel_id\""
 )
 file_name=(
     "labjack_temperature_room"
@@ -87,30 +105,16 @@ file_name=(
     "ctc_voltage"
     "ctc_pressure"
     "ctc_heater"
+    "pacman_boards_vdda_all_tiles"
+    "pacman_boards_vddd_all_tiles"
+    "pacman_boards_idda_all_tiles"
+    "pacman_boards_iddd_all_tiles"
+    "pacman_boards_mean_pedestal_all_tiles"
+    "pacman_boards_packets_all_tiles"
 )
 
 # Loop over desired variables and run the command to create the backup for each of them
 for i in "${chosen_variables[@]}"; do
-    eval "influx query --org \"$org\" --token \"$influx_token_marina\" 'from(bucket:\"$bucket\") |> range($time_interval) |> filter(fn: (r) => ${variables[i]}) |> keep(columns:[${columns[i]}])' --raw > ${dir_path}/${file_name[i]}_${file_name_tag}.csv"
+    eval "influx query --org \"$org\" --token \"$influx_token_marina\" 'from(bucket:\"$bucket\") |> range($time_interval) |> filter(fn: (r) => ${variables[i]}) |> keep(columns:[${columns[i]}])' --raw > ${dir_path}/${file_name_tag}_${file_name[i]}.csv"
     echo ""
 done
-
-
-# ===========================
-# Back up variables from pacman boards
-
-# loop over all tiles, and save vdda, vddd, idda, iddd, as well as channel information such as mean pedestal and packets
-for tile in [1,2,3,4,5,6,7,8]:
-    # vdda
-    eval "influx query --org \"$org\" --token \"$influx_token_marina\" 'from(bucket:\"$bucket\") |> range($time_interval) |> filter(fn: (r) => "r._measurement==\"pacman_boards\" and r._field==\"vdda\" and r.tile==\"${tile}\"") |> keep(columns:["\"_time\",\"_value\",\"_field\",\"_measurement\",\"tile\""])' --raw > ${dir_path}/pacman_vdda_tile${tile}_${file_name_tag}.csv"
-    # vddd
-    eval "influx query --org \"$org\" --token \"$influx_token_marina\" 'from(bucket:\"$bucket\") |> range($time_interval) |> filter(fn: (r) => "r._measurement==\"pacman_boards\" and r._field==\"vddd\" and r.tile==\"${tile}\"") |> keep(columns:["\"_time\",\"_value\",\"_field\",\"_measurement\",\"tile\""])' --raw > ${dir_path}/pacman_vddd_tile${tile}_${file_name_tag}.csv"
-    # idda
-    eval "influx query --org \"$org\" --token \"$influx_token_marina\" 'from(bucket:\"$bucket\") |> range($time_interval) |> filter(fn: (r) => "r._measurement==\"pacman_boards\" and r._field==\"idda\" and r.tile==\"${tile}\"") |> keep(columns:["\"_time\",\"_value\",\"_field\",\"_measurement\",\"tile\""])' --raw > ${dir_path}/pacman_idda_tile${tile}_${file_name_tag}.csv"
-    # iddd
-    eval "influx query --org \"$org\" --token \"$influx_token_marina\" 'from(bucket:\"$bucket\") |> range($time_interval) |> filter(fn: (r) => "r._measurement==\"pacman_boards\" and r._field==\"iddd\" and r.tile==\"${tile}\"") |> keep(columns:["\"_time\",\"_value\",\"_field\",\"_measurement\",\"tile\""])' --raw > ${dir_path}/pacman_iddd_tile${tile}_${file_name_tag}.csv"
-    
-    # mean pedestal
-    eval "influx query --org \"$org\" --token \"$influx_token_marina\" 'from(bucket:\"$bucket\") |> range($time_interval) |> filter(fn: (r) => "r._measurement==\"pacman_boards\" and r._field==\"mean_pedestal\" and r.tile==\"${tile}\"") |> keep(columns:["\"_time\",\"_value\",\"_field\",\"_measurement\",\"tile\",\"channel_id\""])' --raw > ${dir_path}/pacman_mean_pedestal_tile${tile}_all_channels_${file_name_tag}.csv"
-    # packets
-    eval "influx query --org \"$org\" --token \"$influx_token_marina\" 'from(bucket:\"$bucket\") |> range($time_interval) |> filter(fn: (r) => "r._measurement==\"pacman_boards\" and r._field==\"packets\" and r.tile==\"${tile}\"") |> keep(columns:["\"_time\",\"_value\",\"_field\",\"_measurement\",\"tile\",\"channel_id\""])' --raw > ${dir_path}/pacman_packets_tile${tile}_all_channels_${file_name_tag}.csv"
